ce09b85b703968435c29ff06293f8c1401c923b7
Ruslans Tufetulovs <ruslans.tufetulovs@gmail.com>
2020-12-14 17:14:51 +0200

    first-commit

---
+++ src/main/java/com/javaguru/name_lastname_student/lesson_0/task_1/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.javaguru.name_lastname_student.lesson_0.task_1;
+
+class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
